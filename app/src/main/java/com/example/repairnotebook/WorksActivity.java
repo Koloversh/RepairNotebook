1f1be875829da36e2dc5b77876a05e3fefc073dc
koloversh <ImmortalNickolas@yandex.ru>
2021-03-19 22:09:07 +0300

    Small refactoring in worxActivity. Prepaering for double language app.

--- app/src/main/java/com/example/repairnotebook/WorksActivity.java
+++ app/src/main/java/com/example/repairnotebook/WorksActivity.java
@@ -3,9 +3,20 @@ package com.example.repairnotebook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class WorksActivity extends AppCompatActivity{
 
@@ -16,95 +27,24 @@ public class WorksActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.works_activity);
 
-        button = findViewById(R.id.engineButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
+        RecyclerView recyclerView = findViewById(R.id.listOfButtons);
+        WorksAdapter adapter = new WorksAdapter();
+        List<String> items = new ArrayList<>();
+        items.add(getResources().getString(R.string.Engine));
+        items.add(getResources().getString(R.string.Fuel));
+        items.add(getResources().getString(R.string.transmission));
+        items.add(getResources().getString(R.string.FrontAxle));
+        items.add(getResources().getString(R.string.Wheels));
+        items.add(getResources().getString(R.string.Controls));
+        items.add(getResources().getString(R.string.TheBody));
+        items.add(getResources().getString(R.string.Elecrics));
+        items.add(getResources().getString(R.string.RearAxle));
+        items.add(getResources().getString(R.string.Another));
+        items.add(getResources().getString(R.string.Service));
 
-        button = findViewById(R.id.fuelButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.transmissionButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.frontbridgeButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.controlsButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.shellButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.wiresButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.backbridgeButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.anotherThingsButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
-
-        button = findViewById(R.id.serviceButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent worksIntent = new Intent(WorksActivity.this, WorksDataActivity.class);
-                startActivity(worksIntent);
-            }
-        });
+        adapter.setData(items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(adapter);
 
         button = findViewById(R.id.subjectSearchButton);
         button.setOnClickListener(new View.OnClickListener() {
